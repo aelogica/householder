f9218a557bf05afa16f0ca60fbc503d752a1476b
Marvin Dy Matias <marvin.matias@gmail.com>
2013-11-09 14:46:04 +0800

    Use the new method for removing the existing network adapters 1-4.

--- lib/householder.rb
+++ lib/householder.rb
@@ -1,31 +1,25 @@
-require "householder/version"
-require "householder/vbox"
+require_relative "householder/version"
+require_relative "householder/vbox"
 
 module Householder
-  def self.import(appliance_filename)
+  def self.run(raw_argv)
+    box_url, _, remote_host, _, user, _, fqdn, _, ip_address, _, public_key_path = raw_argv
+    appliance_filename = File.basename(box_url)
+
     puts "\nImporting VM...\n\n"
     vm_name = Householder::Vbox.import(appliance_filename)
     vm      = Householder::Vbox.new(vm_name)
+
     puts "\nDone importing VM.\n\n"
 
     puts "Modifying VM configuration:"
-    vm.get_info.each do |key, value|
-      # Remove existing port forwarding rules on Network Adapter 1
-      if /NIC 1 Rule\(\d\)/.match(key)
-        rule_name = /^name = (.+?),/.match(value)
-        vm.remove_port_forwarding_rule(1, rule_name[1]) if !rule_name.nil? && rule_name.size > 1
-
-      # Remove network adapters 3 & 4 to avoid conflict with NAT and Bridged Adapter
-      elsif other_adapters = /^NIC (3|4)$/.match(key) && value != 'disabled'
-        remove_network_adapter(other_adapters[1])
-      end
-    end
+    vm.remove_existing_network_adapters
 
     puts "Creating NAT on Network Adapter 1 and adding port forwarding..."
-    create_nat_with_port_forwarding(1, 2222, 22)
+    vm.create_nat_with_port_forwarding(1, 2222, 22)
 
     puts "Creating Bridged Adapter on Network Adapter 2..."
-    create_bridge_adapter(2, 'eth0')
+    vm.create_bridge_adapter(2, 'eth0')
 
     puts "\nDone modifying VM.\n\n"
   end
